622fbd58048eaf35cf70b912e20cba8397e65518
hayate212 <hayate.jvn@gmail.com>
2021-12-30 00:52:18 +0900

    Update: header の追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ export const App = () => {
   const [softwareList] = useState<GBASoftware[]>(list);
 
   return (
-    <div className="App bg-gray-800 w-full min-h-screen ">
+    <div className="App bg-gray-800 w-full min-h-screen">
+      <header className="bg-gray-800 w-full flex justify-end items-center px-2">
+        <a
+          href="https://github.com/hayate212/gba-savetype-search"
+          target="_blank"
+          className="text-sm text-white border-b border-white hover:text-blue-400 hover:border-blue-400"
+        >
+          repository
+        </a>
+      </header>
       <SoftwareList softwareList={softwareList} />
     </div>
   );
